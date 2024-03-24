22fb49abfe7715032781a5364e1c0d972088144c
ce31062 <tanimura.naoya@jp.panasonic.com>
2024-03-24 20:13:14 +0900

    24th Mar

---
+++ 11.2/test6.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int olympic(int year);
+
+int main(void)
+{
+  int year, hold;
+  printf("西暦を入力してください：");
+  scanf("%d", &year);
+  hold = olympic(year);
+  
+  switch(hold) {
+  case 0:
+    printf("オリンピックは開催されません\n");
+    break;
+  case 1:
+    printf("夏季オリンピックが開催されます\n");
+    break;
+  case 2:
+    printf("冬季オリンピックが開催されます\n");
+    break;
+  };
+  return 0;
+}
+
+ int olympic(int year)
+ {
+   if (year%2==0) {
+     if (year%4==0) {
+       return 1;
+     } else {
+       return 2;
+     }
+   } else {
+       return 0;
+     }
+ }
